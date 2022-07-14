7e119b5be16157ee94f6b8b2a71f27cc704dd753
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-07-14 10:38:08 -0500

    updated description, humidity, and wind

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ function showTemperature(response) {
 let temperature = Math.round(response.data.main.temp);
 let temperatureElement = document.querySelector("#degrees");
 let cityElement = document.querySelector("#current-city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind-speed");
 temperatureElement.innerHTML = `${temperature}°`;
 cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "3fd0b2514fdddeb5a1773faa623df844";
