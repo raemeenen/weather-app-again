206f410ffec987fac8def7dec8de6f41fc0223c8
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-07-16 17:07:02 -0500

    updated time to show AM or PM

--- src/app.js
+++ src/app.js
@@ -15,12 +15,20 @@ function getCurrentLocation(event){
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10) {hours = `0${hours}`};
+
+  let amOrPm = "";
+  if (hours < 12) {amOrPm = " AM";}
+  else if (hours >= 12) {amOrPm = " PM"}
+
+  if (hours <= 12) {hours = hours;}
+  else if (hours > 12) {hours = hours - 12;}
+  else if (hours == 0) {hours = "12";}
+
   let minutes = date.getMinutes();
   if (minutes < 10) {minutes = `0${minutes}`};
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}${amOrPm}`;
 }
 
 function formatDay(timestamp) {
